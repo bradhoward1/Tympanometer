27893d828ed8c732ce655dfebd3d50e166fdb466
bradhoward1 <65506495+bradhoward1@users.noreply.github.com>
2020-07-24 15:00:10 -0400

    Now pump, speaker, and mic work simultaneously
    Cleaned up code and added more functionality to microphone, pump, and speaker.

--- tymp.ino
+++ tymp.ino
@@ -1,13 +1,15 @@
 int SpeakerPin = 5;
-int SpeakerButton = 7;
+int PumpButton = 7;
 int PumpPin = 4;
-int PumpButton = 1;
+int SpeakerButton = 8;
 int soundPin = A0;
+int count = 0;
+int micButton = 13;
 const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
 unsigned int sample;
+
 void setup() {
   // set up Serial Monitor
-  // Serial.begin();
   
   // set up pins
   Serial.begin(9600);
@@ -16,18 +18,53 @@ void setup() {
   pinMode(SpeakerPin, OUTPUT);
   pinMode(PumpButton, INPUT);
   pinMode(soundPin, INPUT);
+  pinMode(micButton, INPUT);
 }
 
 
 void loop() 
 {
-   unsigned long startMillis= millis();  // Start of sample window
+  PumpOutput();
+  SpeakerOutput();
+  micInput();
+}
+
+int PumpOutput()
+{
+    //Particle.variable("cmd", cmd);
+    int PumpButtonState = digitalRead(PumpButton);
+    if (PumpButtonState == HIGH)
+    {
+        digitalWrite(PumpPin, HIGH);
+        bool success;
+    }
+    if (PumpButtonState == LOW)
+    {
+        digitalWrite(PumpPin, LOW);
+        bool success;
+    }
+}
+
+int SpeakerOutput()
+{
+    int buttonState = digitalRead(SpeakerButton);
+    if (buttonState == HIGH)
+    {
+        tone(SpeakerPin, 226);
+    }
+    if (buttonState == LOW)
+    {
+        noTone(SpeakerPin);
+    }
+}
+
+int micInput(){
+    unsigned long startMillis= millis();  // Start of sample window
    unsigned int peakToPeak = 0;   // peak-to-peak level
    unsigned int signalMax = 0;
    unsigned int signalMin = 1024;
  
    // collect data for 50 mS
-   SpeakerOutput();
    while (millis() - startMillis < sampleWindow)
    {
       sample = analogRead(0);
@@ -45,36 +82,6 @@ void loop()
    }
    peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
    float volts = (peakToPeak * 5.0)/ 1024;  // convert to volts
-   Serial.println(volts);
-    //PumpOutput();
-}
-
-int PumpOutput()
-{
-    //Particle.variable("cmd", cmd);
-    int buttonState = digitalRead(PumpButton);
-    if (buttonState == LOW)
-    {
-        digitalWrite(PumpPin, HIGH);
-        bool success;
-    }
-    if (buttonState == HIGH)
-    {
-        digitalWrite(PumpPin, LOW);
-        bool success;
-    }
-}
-
-int SpeakerOutput()
-{
-//    Particle.variable("cmd", cmd);
-    int buttonState = digitalRead(SpeakerButton);
-    if (buttonState == HIGH)
-    {
-        tone(SpeakerPin, 226);
-    }
-    if (buttonState == LOW)
-    {
-        noTone(SpeakerPin);
-    }
+   int button = digitalRead(micButton);
+   Serial.println(peakToPeak);
 }
