80d2b4530844a8a07f92f404a54d83e53983021d
bradhoward1 <bradley.howard@duke.edu>
2020-07-30 19:45:09 -0400

    Spacing issue

--- server_client.py
+++ server_client.py
@@ -6,7 +6,7 @@ server_name = "http://127.0.0.1:5000"
 
 def data():
     data = {"subject": 12,
-             "value": 103}
+            "value": 103}
     r = requests.post(server_name+"/api/add_data", json=data)
     print(r.text)
 
