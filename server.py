4354467c36612b94ee19a4ea45f3ae1c95c8695f
bradhoward1 <bradley.howard@duke.edu>
2020-07-31 15:35:03 -0400

    Changed server code to take in multiple values from Photon

--- server.py
+++ server.py
@@ -32,8 +32,41 @@ def add_data(in_dict):
     for key in keys:
         if key == "subject":
             new_info.subject = in_dict[key]
-        elif key == "value":
+        elif key == "value_1":
             new_info.values = [in_dict[key]]
+        elif key == "value_2":
+            new_info.update({"push": {"values":
+                            in_dict[key]}})
+        elif key == "value_3":
+            new_info.update({"push": {"values":
+                            in_dict[key]}})
+        elif key == "value_4":
+            new_info.update({"push": {"values":
+                            in_dict[key]}})
+        elif key == "value_5":
+            new_info.update({"push": {"values":
+                            in_dict[key]}})
+        elif key == "value_6":
+            new_info.update({"push": {"values":
+                            in_dict[key]}})
+        elif key == "value_7":
+            new_info.update({"push": {"values":
+                            in_dict[key]}})
+        elif key == "value_8":
+            new_info.update({"push": {"values":
+                            in_dict[key]}})
+        elif key == "value_9":
+            new_info.update({"push": {"values":
+                            in_dict[key]}})
+        elif key == "value_10":
+            new_info.update({"push": {"values":
+                            in_dict[key]}})
+        elif key == "value_11":
+            new_info.update({"push": {"values":
+                            in_dict[key]}})
+        elif key == "value_12":
+            new_info.update({"push": {"values":
+                            in_dict[key]}})
     new_info.save()
     return True
 
