733164c7294fa95e719b3d2aa8ae248df961f972
bradhoward1 <bradley.howard@duke.edu>
2020-08-04 15:09:02 -0400

    Function to add new values to the database to new or existing patients

--- server.py
+++ server.py
@@ -101,13 +101,41 @@ def add_data(in_dict):
         add_new_vals(in_dict)
 
 
-
 def add_vals(in_dict):
-    pass
-
+    new_info = SendData.objects.raw({"_id": in_dict["subject"]})
+    new_info.update({"$push": {"values": in_dict["value_1"]}})
+    new_info.update({"$push": {"values": in_dict["value_2"]}})
+    new_info.update({"$push": {"values": in_dict["value_3"]}})
+    new_info.update({"$push": {"values": in_dict["value_4"]}})
+    new_info.update({"$push": {"values": in_dict["value_5"]}})
+    new_info.update({"$push": {"values": in_dict["value_6"]}})
+    new_info.update({"$push": {"values": in_dict["value_7"]}})
+    new_info.update({"$push": {"values": in_dict["value_8"]}})
+    new_info.update({"$push": {"values": in_dict["value_9"]}})
+    new_info.update({"$push": {"values": in_dict["value_10"]}})
+    new_info.update({"$push": {"values": in_dict["value_11"]}})
+    new_info.update({"$push": {"values": in_dict["value_12"]}})
+    new_info.update({"$push": {"values": in_dict["value_13"]}})
+    new_info.update({"$push": {"values": in_dict["value_14"]}})
+    new_info.update({"$push": {"values": in_dict["value_15"]}})
 
 def add_new_vals(in_dict):
-    pass
+    new_info.values = in_dict["value_1"]
+    new_info.save()
+    new_info.update({"$push": {"values": in_dict["value_2"]}})
+    new_info.update({"$push": {"values": in_dict["value_3"]}})
+    new_info.update({"$push": {"values": in_dict["value_4"]}})
+    new_info.update({"$push": {"values": in_dict["value_5"]}})
+    new_info.update({"$push": {"values": in_dict["value_6"]}})
+    new_info.update({"$push": {"values": in_dict["value_7"]}})
+    new_info.update({"$push": {"values": in_dict["value_8"]}})
+    new_info.update({"$push": {"values": in_dict["value_9"]}})
+    new_info.update({"$push": {"values": in_dict["value_10"]}})
+    new_info.update({"$push": {"values": in_dict["value_11"]}})
+    new_info.update({"$push": {"values": in_dict["value_12"]}})
+    new_info.update({"$push": {"values": in_dict["value_13"]}})
+    new_info.update({"$push": {"values": in_dict["value_14"]}})
+    new_info.update({"$push": {"values": in_dict["value_15"]}})
 
 
 @app.route("/api/add_data", methods=["POST"])
