dbe848e84cef01f9cc184f7ea0f8ffa10662398c
bradhoward1 <bradley.howard@duke.edu>
2020-08-09 10:11:34 -0400

    Server function to receive pressure data and add it to database

--- server.py
+++ server.py
@@ -16,6 +16,7 @@ app = Flask(__name__)
 class SendData(MongoModel):
     subject = fields.CharField(primary_key=True)
     values = fields.ListField()
+    pressure_values = fields.ListField()
 
 
 def __init__():
@@ -110,6 +111,68 @@ def get_patient_info():
         return jsonify(values), 200
     else:
         return "Unable to retrieve list of recorded mic values", 400
+
+
+def add_pressure_data(in_dict):
+    new_info = SendData()
+    keys = check_keys(in_dict)
+    try:
+        presence_check = SendData.objects.get({"_id": in_dict["subject"]})
+    except SendData.DoesNotExist:
+        presence_check = False
+    if presence_check is False:
+        add_new_pressure_vals(in_dict)
+    else:
+        add_pressure_vals(in_dict)
+    return True
+
+
+def add_pressure_vals(in_dict):
+    new_info = SendData.objects.raw({"_id": in_dict["subject"]})
+    new_info.update({"$push": {"pressure_values": in_dict["value_1"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_2"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_3"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_4"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_5"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_6"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_7"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_8"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_9"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_10"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_11"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_12"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_13"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_14"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_15"]}})
+
+
+def add_new_pressure_vals(in_dict):
+    new_info = SendData()
+    new_info.subject = in_dict["subject"]
+    new_info.pressure_values = [in_dict["value_1"]]
+    new_info.save()
+    new_info = SendData.objects.raw({"_id": in_dict["subject"]})
+    new_info.update({"$push": {"pressure_values": in_dict["value_2"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_3"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_4"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_5"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_6"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_7"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_8"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_9"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_10"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_11"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_12"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_13"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_14"]}})
+    new_info.update({"$push": {"pressure_values": in_dict["value_15"]}})
+
+
+@app.route("/api/add_pressure", methods=["POST"])
+def post_info():
+    in_dict = request.get_json()
+    add_data(in_dict)
+    return "Good post made to server", 200
 
 
 if __name__ == '__main__':
